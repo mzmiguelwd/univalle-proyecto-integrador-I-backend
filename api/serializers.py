d3f547a1bf3a3fd84f9f8c28dc5e219a037fc8a2
mzmiguelwd <mzmiguel.wd@gmail.com>
2026-02-27 05:42:56 -0500

    fix: user read only field

--- api/serializers.py
+++ api/serializers.py
@@ -135,4 +135,4 @@ class TaskSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
         ]
-        read_only_fields = ['created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at', 'user']
